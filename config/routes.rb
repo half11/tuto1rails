0e90b9bfd894a87352eec4eecfc4004bcae9d27f
alfredCM <alfredo.castillonmacias@students.fhnw.ch>
2019-06-26 19:03:12 -0500

    usando controlador y variable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'prueba', to: 'prueba#metodoprueba'
+  #get 'prueba', to: 'prueba#metodoprueba'
+  get 'probar', to: 'control#saludo'
 end
