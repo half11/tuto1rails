675a22de81f3207012cdf84397acb8adea814d70
alfredCM <alfredo.castillonmacias@students.fhnw.ch>
2019-06-27 23:41:13 -0500

    guardar vista-controlador-modelo

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,16 @@ class ImagesController < ApplicationController
 
     def new 
     end
+
+    def create
+        #render plain: params[:image].inspect
+        @image = Image.new image_params
+    end
+
+    private
+
+    def image_params
+        params.require(:image).permit(:description)
+    end
+
 end
