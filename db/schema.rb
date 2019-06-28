c53dbaf76f5fe3c8263a8ec8d23ee13c32894d4c
alfredCM <alfredo.castillonmacias@students.fhnw.ch>
2019-06-27 23:21:04 -0500

    modelo, vista y controlador con su ruta

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_06_28_035130) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "description"
+    t.integer "likes_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
