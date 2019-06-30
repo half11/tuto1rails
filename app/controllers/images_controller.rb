e569867ba368a05b0e95ff0c451fb40fee46bb41
alfredCM <alfredo.castillonmacias@students.fhnw.ch>
2019-06-29 21:07:04 -0500

    db fixed and rendering

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,17 +1,27 @@
 class ImagesController < ApplicationController
 
+    def index
+        @images = Image.all
+    end
     def new 
     end
 
     def create
         #render plain: params[:image].inspect
         @image = Image.new image_params
+        @image.save
+
+        redirect_to @image
     end
 
     private
 
     def image_params
         params.require(:image).permit(:description)
+    end
+
+    def show
+        @image = Image.find params[:id]
     end
 
 end
