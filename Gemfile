4a737c91c6cfac93313bfe9eea9f400fee0395fc
alfredCM <alfredo.castillonmacias@students.fhnw.ch>
2019-07-06 20:23:15 -0500

    gem devise for user auth

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby '2.5.5'
 #To upload images
 gem 'carrierwave'
 gem 'mini_magick'
+
+gem 'devise', git: 'git://github.com/plataformatec/devise.git'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #get 'prueba', to: 'prueba#metodoprueba'
+  root to: "home#index"
   resources :images
 
   get 'probar', to: 'control#saludo'
