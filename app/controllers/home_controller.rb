29884a8032bb74391369eac12af391a215b734f1
alfredCM <alfredo.castillonmacias@students.fhnw.ch>
2019-07-13 19:59:39 -0500

    fixed view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      @images = Image.paginate(page: params[:page], per_page: 3)
+      @images = Image.paginate(page: params[:page], per_page: 6)
         .order(id: :desc)
         .where user_id: current_user.id
     end
